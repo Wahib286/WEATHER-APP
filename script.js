83541085d07927c6cd5d49fdad712e12f40189ef
Wahib <Wahibbashar.it25@jecrc.ac.in>
2025-03-14 02:11:35 +0530

    js done

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 let currentTab = userTab;
 const API_KEY = "3b660790a882d954a35fdd870a5eb3e3";
 currentTab.classList.add("current-tab");
+getfromSessionStorage();
 
 function switchTab(clickedTab){
     if(clickedTab != currentTab){
@@ -51,14 +52,117 @@ function switchTab(clickedTab){
     }
 
     if(!searchForm.classList.contains("active")){
+        searchForm.classList.add("active");
         userContainer.classList.remove("active");
         grantAccessContainer.classList.remove("active");
-        searchForm.classList.add("active");
     }
     else{
-        
+        searchForm.classList.remove("active");
+        userInfoContainer.classList.remove("active");
+        getfromSessionStorage();
     }
 }
+
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates){
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates){
+    const {lat , lon} = coordinates;
+    grantAccessContainer.classList.remove("active");
+    loadingScreen.classList.add("active");
+    //API call
+    try{
+        const response = await fetch(`https://api.weatherstack.com/current?access_key=${API_KEY}&query=${lat},${lon}`);
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+    }
+
+}
+
+function renderWeatherInfo(weatherInfo){
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windSpeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    cityName.innerText = weatherInfo?.location?.name;
+    // countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.location?.country.toLowerCase()}.png`;
+    //Flag ke liye mehnat
+    const countryName = weatherInfo?.location?.country;
+    fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+      .then(response => response.json())
+      .then(data => {
+          const flagId = data[0]?.cca2;
+          if(flagId) {
+              countryIcon.src =`https://flagcdn.com/144x108/${flagId.toLowerCase()}.png`;
+          } else {
+              console.log("Error aaya hai")
+          }
+      })
+    //mehnat over
+    desc.innerText = weatherInfo?.current?.weather_descriptions?.[0]; 
+    weatherIcon.src = weatherInfo?.current?.weather_icons?.[0]; 
+    temp.innerText = `${weatherInfo?.current?.temperature}°C`; 
+    windSpeed.innerText = `${weatherInfo?.current?.wind_speed} km/h`;
+    humidity.innerText = `${weatherInfo?.current?.humidity}%`; 
+    cloudiness.innerText = `${weatherInfo?.current?.cloudcover}%`; 
+} 
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        alert("No Geolocation support available");
+    }
+}
+
+function showPosition(position){
+
+    const usercoordinates = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+
+    sessionStorage.setItem("user-coordinates", JSON.stringify(usercoordinates) );
+}
+
+async function fetchSearchWeatherInfo(city){
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+
+    try{
+        const response = await fetch(
+            `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`
+        );
+
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err){
+        console.log("error aaya hai at fetchSearchWeatherInfo");
+    }
+}
+
 userTab.addEventListener("click", ()=>{
     switchTab(userTab);
 });
@@ -66,3 +170,17 @@ userTab.addEventListener("click", ()=>{
 searchTab.addEventListener("click", ()=>{
     switchTab(searchTab);
 });
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click", getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let cityName = searchInput.value;
+    if(cityName === "") 
+        return;
+    else{
+        fetchSearchWeatherInfo(cityName);
+    }
+})
